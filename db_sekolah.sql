0ccd51cc5cde8522f3e987ae0b56ee7e11b30b67
julfan-laia <julfanlaia162@gmail.com>
2025-07-13 23:03:15 +0700

    Add files via upload

--- db_sekolah.sql
+++ db_sekolah.sql
@@ -1,10 +1,68 @@
-CREATE DATABASE IF NOT EXISTS db_sekolah;
-USE db_sekolah;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Jul 2025 pada 17.49
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
 
-CREATE TABLE IF NOT EXISTS siswa (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR(100),
-    alamat TEXT,
-    jenis_kelamin VARCHAR(10),
-    tanggal_lahir DATE
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `jenis_kelamin` varchar(10) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `siswa`
+--
+
+INSERT INTO `siswa` (`id`, `nama`, `alamat`, `jenis_kelamin`, `tanggal_lahir`) VALUES
+(1, 'julfan laia', 'batu aji ', 'Laki-laki', '2025-06-26');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
